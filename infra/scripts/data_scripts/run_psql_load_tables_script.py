0435738d30e994593cfcb1c7f58bad84af987a5c
Gaiye Zhou <gazho@microsoft.com>
2025-03-13 22:26:52 -0400

    test

--- infra/scripts/data_scripts/run_psql_load_tables_script.py
+++ infra/scripts/data_scripts/run_psql_load_tables_script.py
@@ -1,170 +1,104 @@
 from azure.identity import DefaultAzureCredential
 import psycopg2
-from psycopg2 import sql
-# for data loading 
 import os
 import pandas as pd
 
-
+# Configuration parameters
 key_vault_name = "key_vault_name_place_holder"
 host_name = "host_name_place_holder"
 admin_principal_name = "admin_principal_name_place_holder"
 identity_name = "identity_name_place_holder"
-database_name= "database_name_place_holder"
+database_name = "database_name_place_holder"
 basrUrl = "basrUrl_place_holder"
 
-
-# Grant Permission Function
-def grant_permissions(cursor, db_name, schema_name, principal_name):
+def load_table_from_csv(cursor, table_name, csv_file_path, columns):
     """
-    Grants database and schema-level permissions to a specified principal.
+    Loads data from a CSV file into a specified PostgreSQL table.
+    
     Parameters:
     - cursor: psycopg2 cursor object for database operations.
-    - db_name: Name of the database to grant CONNECT permission.
-    - schema_name: Name of the schema to grant table-level permissions.
-    - principal_name: Name of the principal (role or user) to grant permissions.
+    - table_name: Name of the PostgreSQL table to load data into.
+    - csv_file_path: Path to the CSV file containing the data.
+    - columns: List of column names in the table.
     """
-    # Check if the principal exists in the database
-    cursor.execute(
-        sql.SQL("SELECT 1 FROM pg_roles WHERE rolname = {principal}").format(
-            principal=sql.Literal(principal_name)
-        )
-    )
-    if cursor.fetchone() is None:
-        add_principal_user_query = sql.SQL(
-            "SELECT * FROM pgaadauth_create_principal({principal}, false, false)"
-        )
-        cursor.execute(
-            add_principal_user_query.format(
-                principal=sql.Literal(principal_name),
-            )
-        )
+    df = pd.read_csv(csv_file_path)
+    df.columns = df.columns.str.strip()
+    rows = [tuple(row[col] for col in columns) for index, row in df.iterrows()]
+    insert_query = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES %s"
+    psycopg2.extras.execute_values(cursor, insert_query, rows)
 
-    # Grant CONNECT on database
-    grant_connect_query = sql.SQL("GRANT CONNECT ON DATABASE {database} TO {principal}")
-    cursor.execute(
-        grant_connect_query.format(
-            database=sql.Identifier(db_name),
-            principal=sql.Identifier(principal_name),
-        )
-    )
-    print(f"Granted CONNECT on database '{db_name}' to '{principal_name}'")
-
-    # Grant SELECT, INSERT, UPDATE, DELETE on schema tables
-    grant_permissions_query = sql.SQL(
-        "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA {schema} TO {principal}"
-    )
-    cursor.execute(
-        grant_permissions_query.format(
-            schema=sql.Identifier(schema_name),
-            principal=sql.Identifier(principal_name),
-        )
-    )
-# end of grant_permissions function
-
-def load_tables(cursor, csv_file_dir):
+def load_tables(conn, cursor, csv_file_paths):
     """
-    load tables from the data files in this directory basrUrl/data/postgresql_db_sample_data
-    example basrUrl: 'https://raw.githubusercontent.com/gitHubUserName/RepositoryName/main/infra/'
-    csv_file_dir: 'https://raw.githubusercontent.com/gitHubUserName/RepositoryName/main/infra/data/postgresql_db_sample_data'
-    data files are:
-    - products.csv
-    - orders.csv
-    - customers.csv
-    """
-    # Path to the file
-    csv_file_path_products = os.path.join(csv_file_dir, 'products.csv')
-    csv_file_path_orders = os.path.join(csv_file_dir, 'orders.csv')
-    csv_file_path_customers = os.path.join(csv_file_dir, 'customers.csv')
-
-    # Read the CSV file into a DataFrame
-    df_products = pd.read_csv(csv_file_path_products)
-    df_orders = pd.read_csv(csv_file_path_orders)
-    df_customers = pd.read_csv(csv_file_path_customers)
-
-    # Strip whitespace from column names
-    df_products.columns = df_products.columns.str.strip()
-    df_customers.columns = df_customers.columns.str.strip()
-    df_orders.columns = df_orders.columns.str.strip()
+    Loads data from CSV files into PostgreSQL tables.
     
-        # Insert data into the products table
-    for index, row in df_products.iterrows():
-        cursor.execute(
-            "INSERT INTO products (id, product_name, price, category, brand, product_description) "
-            "VALUES (%s, %s, %s, %s, %s, %s)",
-            (
-                row['id'],
-                row['product_name'],
-                row['price'],
-                row['category'],
-                row['brand'],
-                row['product_description']
-            )
-        )
-        
-        
-        # Insert data into the customers table
-    for index, row in df_customers.iterrows():
-        cursor.execute(
-            "INSERT INTO customers (id, first_name, last_name, gender, date_of_birth, age, email, phone, post_address, membership) "
-            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-            (
-                row['id'],
-                row['first_name'],
-                row['last_name'],
-                row['gender'],
-                row['date_of_birth'],
-                row['age'],
-                row['email'],
-                row['phone'],
-                row['post_address'],
-                row['membership']
-            )
-        )
-    #
-    # Insert data into the orders table
-    for index, row in df_orders.iterrows():
-        cursor.execute(
-            "INSERT INTO public.orders (id, customer_id, product_id, quantity, total, order_date, customer_first_name, customer_last_name, unit_price, category, brand, product_description, return_status) "
-            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-            (
-                row['id'],
-                row['customer_id'],
-                row['product_id'],
-                row['quantity'],
-                row['total'],
-                row['order_date'],
-                row['customer_first_name'],
-                row['customer_last_name'],
-                row['unit_price'],
-                row['category'],
-                row['brand'],
-                row['product_description'],
-                row['return_status']
-            )
-        )
-        
-    # Commit the changes to the database
+    Parameters:
+    - conn: psycopg2 connection object.
+    - cursor: psycopg2 cursor object for database operations.
+    - csv_file_paths: Dictionary containing the paths to the CSV files.
+    """
+    # Load data into the products table
+    print("Loading data into the products table...")
+    load_table_from_csv(
+        cursor, 'products', csv_file_paths['products'], 
+        ['id', 'product_name', 'price', 'category', 'brand', 'product_description']
+    )
     conn.commit()
-    print("Data loaded successfully into the tables.")
-    
+    print("Data loaded successfully into the products table.")
+
+    # Load data into the customers table
+    print("Loading data into the customers table...")
+    load_table_from_csv(
+        cursor, 'customers', csv_file_paths['customers'], 
+        [
+            'id', 'first_name', 'last_name', 'gender', 'date_of_birth', 
+            'age', 'email', 'phone', 'post_address', 'membership'
+        ]
+    )
+    conn.commit()
+    print("Data loaded successfully into the customers table.")
+
 ##########################################################################################
 # Program starts here
 ##########################################################################################
-# Acquire the access token
-cred = DefaultAzureCredential()
-access_token = cred.get_token("https://ossrdbms-aad.database.windows.net/.default")
+try:
+    # Acquire the access token
+    print("Acquiring access token...")
+    cred = DefaultAzureCredential()
+    access_token = cred.get_token("https://ossrdbms-aad.database.windows.net/.default")
+    print("Access token acquired.")
 
-# Combine the token with the connection string to establish the connection.
-# The identity name is used as the username in the connection string.
-conn_string = "host={0} user={1} dbname={2} password={3} sslmode=require".format(
-    host_name, identity_name, database_name, access_token.token
-)
-conn = psycopg2.connect(conn_string)
-cursor = conn.cursor()
+    # Combine the token with the connection string to establish the connection.
+    print("Establishing database connection...")
+    conn_string = (
+        "host={0} user={1} dbname={2} password={3} sslmode=require".format(
+            host_name, identity_name, database_name, access_token.token
+        )
+    )
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    print("Database connection established.")
 
-csv_file_path = os.path.join(basrUrl, 'data/postgresql_db_sample_data')
-load_tables(cursor, csv_file_path)
+    # Construct the paths to the CSV files
+    csv_file_paths = {
+        'customers': os.path.join(basrUrl, 'data/postgresql_db_sample_data', 'customers.csv')
+    }
 
-cursor.close()
-conn.close()
+    # Construct the paths to the CSV files
+    # csv_file_paths = {
+    #     'products': os.path.join(basrUrl, 'data/postgresql_db_sample_data', 'products.csv'),
+    #     'customers': os.path.join(basrUrl, 'data/postgresql_db_sample_data', 'customers.csv'),
+    #     'orders': os.path.join(basrUrl, 'data/postgresql_db_sample_data', 'orders.csv')
+    # }
+    
+
+    # Load tables
+    load_tables(conn, cursor, csv_file_paths)
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+    conn.rollback()  # Rollback the transaction in case of error
+finally:
+    if cursor:
+        cursor.close()
+    if conn:
+        conn.close()
